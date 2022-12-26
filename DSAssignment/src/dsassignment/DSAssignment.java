2e4ee2ecb2412c871fa91703d8da08d3ef5c8574
moham <moham@Mohammad-PC.home>
2022-12-26 02:16:50 +0200

    add student class

--- DSAssignment/src/dsassignment/DSAssignment.java
+++ DSAssignment/src/dsassignment/DSAssignment.java
@@ -1,21 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dsassignment;
 
-/**
- *
- * @author moham
- */
+class Student {
+private int Number;
+private String Name;
+private String Major;
+private double GPA;
+
+    public Student(int Number, String Name, String Major, double GPA) {
+        this.Number = Number;
+        this.Name = Name;
+        this.Major = Major;
+        this.GPA = GPA;
+    }
+
+    public void setNumber(int Number) {
+        this.Number = Number;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+
+
+
+}
 public class DSAssignment {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
